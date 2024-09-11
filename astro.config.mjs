edcd2881f546f2741e73a686c4bd8eedfe179242
Andres Araque <aaraqueamaya397@gmail.com>
2024-09-11 13:00:35 -0500

    [ADD] Creating Header With React

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react()]
 });
