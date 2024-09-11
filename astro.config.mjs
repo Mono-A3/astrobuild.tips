a497b3f94bd442c4b9bd49c32e73f560161be814
Andres Araque <aaraqueamaya397@gmail.com>
2024-09-11 13:09:57 -0500

    [ADD] Creating Footer With Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react(), vue()]
 });
